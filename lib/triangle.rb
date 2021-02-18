0552431efb30e79940ec50e5bcc9f627ec0ac325
Sara Nolen <nolensara1@gmail.com>
2021-02-18 23:54:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,18 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
 
-  def initialize(side1:, side2:, side3:)
+  def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
-
-    if side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2)
-      raise TriangleError
-    else
-    end
-
-
-
   end
 
+  def triangle?
+    if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2) ||
+      side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    end
+  end
   class TriangleError < StandardError
   end
 end
