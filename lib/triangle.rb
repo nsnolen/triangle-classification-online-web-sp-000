e2806ebf02fd5d04a796c9b0e4741f14a20bb6b7
Sara Nolen <nolensara1@gmail.com>
2021-02-18 23:50:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,8 +6,12 @@ class Triangle
     @side2 = side2
     @side3 = side3
 
-    if side1 > (side2 + side3)
+    if side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2)
       raise TriangleError
+    else 
+      
+  
+      
   end
 
   class TriangleError < StandardError
