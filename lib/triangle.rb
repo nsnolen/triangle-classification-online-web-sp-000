ce79d7aa6b311cb1ad615d59a2eec522bfc858c9
Sara Nolen <nolensara1@gmail.com>
2021-02-19 00:35:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,10 +12,10 @@ class Triangle
 
     real_triangle = [(side1 < side2 + side3), (side2 < side1 + side3), (side3 < side2 + side1)
 
-    [side1, side2, side3] each{|side| real_triangle << false if side <= 0}
+    [side1, side2, side3].each{|side| real_triangle << false if side <= 0}
 
     raise TriangleError if real_triangle.include?(false)
-  end
+
   end
 
   class TriangleError < StandardError
