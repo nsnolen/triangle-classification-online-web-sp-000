e3bec78cefeea67b880427defbc1c2ef32167fa7
Sara Nolen <nolensara1@gmail.com>
2021-02-19 00:06:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,17 +10,14 @@ class Triangle
 
   def valid_triangle?
     sides.any?{|side| side <= 0} || sides[0] + sides[1] <= sides[2]
-
-    if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2) ||
-      side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 == nil || side2 == nil || side3 == nil
-      raise TriangleError
-    end
   end
   class TriangleError < StandardError
   end
 
   def kind
-    if side1 == side2 && side1 == side3
+    if valid_triangle?
+      raise Triangle_Error 
+    elsif side1 == side2 && side1 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
       :isosceles
