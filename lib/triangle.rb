dfdeda9fd6aab64b684e6ef05376c81552ae7b96
Sara Nolen <nolensara1@gmail.com>
2021-02-19 00:10:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,6 @@ class Triangle
         if side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2) ||
           side1 <= 0 || side2 <= 0 || side3 <= 0
           raise TriangleError
-
         end
   end
   class TriangleError < StandardError
