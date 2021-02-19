d7d1d28e3ca8d4611bf51d8e4219d3825b55f0a3
Sara Nolen <nolensara1@gmail.com>
2021-02-19 00:27:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if side1.positive? && side2.positive? && side3.positive? && side1 < (side2 + side3) || side2 < (side1 + side3) || side3 < (side2 + side1)
       true
     else
-      false
+      raise TriangleError 
     end
   end
 
@@ -20,8 +20,7 @@ class Triangle
   end
 
   def kind
-    if valid_triangle?(false)
-      raise TriangleError
+    if valid_triangle?
     elsif side1 == side2 && side1 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
