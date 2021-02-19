35e9ef4a21a61edfc32d5dd253001384c129cc5b
Sara Nolen <nolensara1@gmail.com>
2021-02-19 00:04:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
 
   def valid_triangle?
     sides.any?{|side| side <= 0} || sides[0] + sides[1] <= sides[2]
-    
+
     if side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2) ||
       side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 == nil || side2 == nil || side3 == nil
       raise TriangleError
